4659e97bdec4382578293c6b609ff5f7fe9d1231
andreyLobo0 <andrey.lobo@luby.software>
2025-05-12 15:47:58 -0300

    ✨ Feat: Criação de um Setup de Autenticação para os testes autenticados

---
+++ src/utils/auth.setup.ts
@@ -0,0 +1,21 @@
+import { test as setup} from '@playwright/test';
+import { AdminUser } from '../actors/AdminUser';
+import { LoginAsAdmin } from '../tasks/LoginAsAdmin';
+import { IsPageVisible } from '../questions/IsPageVisible';
+import path from 'path';
+
+const authFile = path.join(__dirname, '../../src/utils/.auth/user.json');
+
+setup('authenticate', async ({ page }) => {
+    // Arrange
+    await page.goto('https://front.serverest.dev/login');
+    const user = new AdminUser(page);
+    // Act
+    await user.attemptTo(LoginAsAdmin('fulano@qa.com', 'teste'));
+    // Assert
+    await page.waitForURL(/.*admin\/home/);
+    await IsPageVisible('admin/home', 'Este é seu sistema para administrar seu ecommerce.');
+    const state = await page.context().storageState();
+    console.log(JSON.stringify(state, null, 2));         
+    await page.context().storageState({ path: authFile });
+});
